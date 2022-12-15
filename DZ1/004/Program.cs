1e1599419f06e80c3142a12e00b851ed0ce5e78a
GeliumTap <116411142+GeliumTap@users.noreply.github.com>
2022-12-16 02:12:44 +0300

    C горем пополам и подсказками

---
+++ DZ1/004/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine() ?? "");
+int i = 1;
+bool not = true;
+
+Console.WriteLine($"Чётные числа от 1 до {number}");
+Console.Write($"{number} -> ");
+
+while (i <= number)
+{
+if (i % 2 != 1)
+    {
+        Console.Write(i + ", ");
+        not = false;
+    }
+    i = i + 1;
+}
+
+if (not)
+{
+    Console.WriteLine("Нет чётных чисел!");
+}
