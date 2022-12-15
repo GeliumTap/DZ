104f73bde57e5f64153ab6ccc170a51b2a73cc42
GeliumTap <116411142+GeliumTap@users.noreply.github.com>
2022-12-16 01:26:16 +0300

    Домашнее задание Завершено

--- DZ1/003/Program.cs
+++ DZ1/003/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+if (number %2 == 0)
+{
+    Console.WriteLine($"{number} -> Yes");
+}
+
+else
+{
+    Console.WriteLine($"{number} -> No");
+}
+
+
+//if (number %2 == 1)
+//{
+//    Console.WriteLine("Число " + number + " является: НЕЧЁТНЫМ");
+//}
+//else
+//{
+//    Console.WriteLine("Число " + number + " является: ЧЁТНЫМ");
+//}
