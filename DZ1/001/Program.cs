15d2e0a7aa09f41f9b5c39e60c6da3591da47834
GeliumTap <116411142+GeliumTap@users.noreply.github.com>
2022-12-15 23:59:35 +0300

    mini fix

--- DZ1/001/Program.cs
+++ DZ1/001/Program.cs
@@ -5,10 +5,11 @@ Console.WriteLine("Введите второе число: ");
 int number_B = int.Parse(Console.ReadLine() ?? "");
 
 if (number_A > number_B)
+
 {
-    Console.WriteLine("Первое число " + number_A + " больше чем второе " + number_B);
+    Console.WriteLine($"{number_A} > {number_B}");
 }
 else
 {
-    Console.WriteLine("Второе число " + number_B + " больше чем первое " + number_A);
+    Console.WriteLine($"{number_B} > {number_A}");
 }
