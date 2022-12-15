f430508cbc08613aca415c1ad90ea0780966ea21
GeliumTap <116411142+GeliumTap@users.noreply.github.com>
2022-12-15 23:34:55 +0300

    Завершения первой задачи.

--- DZ1/001/Program.cs
+++ DZ1/001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int number_A = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine("Введите второе число: ");
+int number_B = int.Parse(Console.ReadLine() ?? "");
+
+if (number_A > number_B)
+{
+    Console.WriteLine("Первое число " + number_A + " больше чем второе " + number_B);
+}
+else
+{
+    Console.WriteLine("Второе число " + number_B + " больше чем первое " + number_A);
+}
