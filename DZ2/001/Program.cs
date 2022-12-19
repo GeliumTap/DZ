5356d01b24ac79326c3ee7df3ebc13b2dc703332
GeliumTap <116411142+GeliumTap@users.noreply.github.com>
2022-12-20 01:48:37 +0300

    Program

--- DZ2/001/Program.cs
+++ DZ2/001/Program.cs
@@ -1,9 +1,29 @@
 ﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе 
 показывает вторую цифру этого числа.*/
 
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount != 3)
+{
+    Console.WriteLine("No");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
+int InCenter(int a)
+{
+    int result = ((a / 10) % 10);
+    return result;
+}
+
+
+/*Рандом от -1000 до 1000 
 var rnd = new Random();
-int num = rnd.Next(-10, 1000);
+int num = rnd.Next(-1000, 1000);
 
-Console.WriteLine($"{num} -> {num / 10 % 10 } ");
+Console.WriteLine($"{num} -> {(num / 10) % 10 } ");
 
-Console.WriteLine();
+Console.WriteLine();*/
---
+++ DZ2/002/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Напишите программу, которая выводит 
+третью цифру заданного числа или сообщает, что третьей цифры нет.*/
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+int count = number.ToString().Length;
+
+Console.Write(MakeArray(number, count));
+
+int MakeArray(int a, int b)
+{
+    int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Потеряли число, держи: ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+    return result;
+
+}
+
+
+/*Рандом от -1000 до 1000
+var rnd = new Random();
+int num = rnd.Next(-1000, 1000);
+
+Console.WriteLine($"{num} -> {num % 10}");
+
+Console.WriteLine();*/
