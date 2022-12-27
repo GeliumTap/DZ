7c991583fef6e7de746fa80f903fbcb09d0c6d49
GeliumTap <116411142+GeliumTap@users.noreply.github.com>
2022-12-28 00:04:59 +0300

    Program Fix 2.0

--- DZ3/003/Program.cs
+++ DZ3/003/Program.cs
@@ -5,8 +5,8 @@
 
 int number = ReadInt("Введите число: ");
 
-for (int i = 1; i <= number; i++){
-    Console.Write($"{i * i * i}. ");
+for (int i = 1; i <= number; i++) {
+    Console.Write($"{i * i * i} ");
 }
 
 int ReadInt(string message){
