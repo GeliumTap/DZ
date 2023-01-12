e92c5e9d5a5304305a2297ca4fa2d5498596fe08
GeliumTap <116411142+GeliumTap@users.noreply.github.com>
2023-01-13 00:55:00 +0300

    Fixet

--- DZ4/003/Program.cs
+++ DZ4/003/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] mass = new int[8];
+
+Console.Write($"{Array} -> [");
+
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(1, 9);
+    Console.Write(" " + Method(i) + ".");
+}
+Console.Write(" ]");
+
+int Method(int a)
+{
+    return mass[a];
+}
