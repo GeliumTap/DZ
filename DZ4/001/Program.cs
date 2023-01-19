dc0398835239232beda08b558ec3d4bc7ca51ed1
GeliumTap <116411142+GeliumTap@users.noreply.github.com>
2023-01-19 18:36:35 +0300

    Fix Program

--- DZ4/001/Program.cs
+++ DZ4/001/Program.cs
@@ -2,25 +2,27 @@
 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 3, 5 -> 243 (3⁵);   2, 4 -> 16   */
 
-int numA = ReadInt("Enter a Number: ");
-int numB = ReadInt("Enter the Degree: ");
-ToDegree(numA, numB);
+Console.Clear();
 
+//____________________________________Возведения в степень_______________________
 
-// Функция возведения в степень
-void ToDegree(int a, int b)
+void Degree(int a, int b)
 {
     int result = 1;
     for (int i = 1; i <= b; i++)
     {
         result = result * a;
     }
-    Console.WriteLine(a + "^" + b + " = " + result);
+    Console.WriteLine(a + "^" + b + " -> " + result);
 }
 
-// Функция ввода
+//_____________________________________Ввод_______________________________________
 int ReadInt(string message)
 {
     Console.WriteLine(message);
     return Convert.ToInt32(Console.ReadLine());
 }
+
+int numA = ReadInt("Enter a Number: ");
+int numB = ReadInt("Enter the Degree: ");
+Degree(numA, numB);
--- DZ4/002/Program.cs
+++ DZ4/002/Program.cs
@@ -1,10 +1,38 @@
-﻿Console.Write("Введите любое число: ");
-enterNumber = Convert.ToInt32(Console.ReadLine());
-result = 0;
-while (enterNumber != 0)
+﻿/* Задача 27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11;  82 -> 10;  9012 -> 12   */
+
+int number = ReadInt("Введите число: ");
+
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+// Функция подсчета цифр в числе
+int NumberLen(int a)
 {
-    result += enterNumber % 10;
-    enterNumber /= 10;
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
 }
 
-Console.WriteLine($"Сумма цифр в числе = {result}");
+// Функция вывода суммы цифр в числе
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"сумма цифр {sum}");
+}
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
