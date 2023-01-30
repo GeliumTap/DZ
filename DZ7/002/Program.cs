3ba338e9becaceef80d7b802176c48dd3d1a1176
GeliumTap <116411142+GeliumTap@users.noreply.github.com>
2023-01-30 18:24:10 +0300

    DZ7

---
+++ DZ7/002/Program.cs
@@ -0,0 +1,69 @@
+﻿Console.Clear();
+/*
+Задача 50: Напишите программу, которая на вход принимает позиции элемента в
+двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+*/
+
+//_____________________________________Запрос у пользователя______________________
+
+Console.WriteLine("введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[10, 10];
+
+
+//_____________________________________Результат___________________________________
+
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n - 1, m - 1]}");
+}
+
+//_____________________________________Методы_____________________________________
+
+//_____________________________________Заполнение массива_________________________
+
+
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100) / 10;
+        }
+    }
+}
+
+//_____________________________________Вывод массива______________________________
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+//_____________________________________Ввод_______________________________________
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ DZ7/003/Program.cs
@@ -0,0 +1,70 @@
+﻿Console.Clear();
+/*
+Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+*/
+
+//_____________________________________Запрос у пользователя______________________
+
+Console.WriteLine("введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+
+//_____________________________________Результат___________________________________
+
+int[,] numbers = new int[n, m];
+
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+
+//_____________________________________Методы_____________________________________
+
+//_____________________________________Заполнение массива_________________________
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+//_____________________________________Вывод массива______________________________
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+//_____________________________________Ввод_______________________________________
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
